b7a693f862392fd822d642c6ea8e1401b965937c
Jinjing Wu <jinjing.wu@compass.com>
2024-03-27 23:54:51 +0100

    feat: setup route handlers test

---
+++ app/api/todo/route.test.ts
@@ -0,0 +1,19 @@
+import { describe, beforeEach, expect, test } from "vitest";
+import { GET } from "./route";
+import { TEST_BASE_URL } from "../../util";
+
+describe("Todo Route Handlers", () => {
+  let url: URL | null = null;
+  beforeEach(() => {
+    url = new URL("/", TEST_BASE_URL);
+  });
+
+  test("should return Todos!", async () => {
+    const req = new Request(url!);
+    const res = await GET(req);
+
+    const body = await res.text();
+    expect(body).toEqual("Todos!");
+    expect(res.status).toBe(200);
+  });
+});
