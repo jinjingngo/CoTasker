e964f40aad019a9ad53fedcf245aa599cc63f186
Jinjing Wu <jinjing.wu@compass.com>
2024-04-03 14:55:31 +0200

    feat: indivitual Streamer instance for each todo list

--- co-tasker-stream/src/Streamer.ts
+++ co-tasker-stream/src/Streamer.ts
@@ -1,34 +1,120 @@
+import type {
+  Command,
+  CreatedTaskStreamPayload,
+  DeletedTaskStreamPayload,
+  OperationFunction,
+  UpdatingTaskStreamPayload,
+} from '../../shared/types';
+
+/**
+ * Sending the updates to the REST API then broadcasting the updated task
+ * 	if got any error abandon the broadcast
+ * @param {UpdatingTaskStreamPayload} payload
+ * @param {WebSocket[]} clients
+ */
+const updatingTaskOperation = (
+  payload: UpdatingTaskStreamPayload,
+  clients: WebSocket[],
+) => {
+  // TODO: send the updates to the REST API
+  console.log('[Broadcast > updatingTaskOperation] message: ', payload);
+  clients.forEach((client) => {
+    client.send(JSON.stringify(payload));
+  });
+};
+
+/**
+ * Broadcast the created task
+ * @param {CreatedTaskStreamPayload} payload
+ * @param {WebSocket[]} clients
+ */
+const createdTaskOperation = (
+  payload: CreatedTaskStreamPayload,
+  clients: WebSocket[],
+) => {
+  console.log('[Broadcast > createdTaskOperation] message: ', payload);
+  clients.forEach((client) => {
+    client.send(JSON.stringify(payload));
+  });
+};
+
+/**
+ * Broadcast the deleted task
+ * @param {DeletedTaskStreamPayload} payload
+ * @param {WebSocket[]} clients
+ */
+const deletedTaskOperation = (
+  payload: DeletedTaskStreamPayload,
+  clients: WebSocket[],
+) => {
+  console.log('[Broadcast > deletedTaskOperation] message: ', payload, clients);
+  clients.forEach((client) => {
+    client.send(JSON.stringify(payload));
+  });
+};
+
+const operations: Record<Command, OperationFunction> = {
+  CREATED_TASK: createdTaskOperation,
+  UPDATING_TASK: updatingTaskOperation,
+  DELETED_TASK: deletedTaskOperation,
+};
+
 export class Streamer {
-	private clients = new Set<WebSocket>();
+  private clients = new Set<WebSocket>();
 
-	constructor(state: DurableObjectState, env: Env) {
-		console.log({ state, env });
-	}
+  constructor(state: DurableObjectState, env: Env) {
+    // TODO: maybe we can have sockets hibernation here
+    console.log({ state, env });
+  }
 
-	async fetch(request: Request) {
-		const url = new URL(request.url);
-		if (request.headers.get('Upgrade') !== 'websocket') {
-			return new Response('Not a WebSocket request', { status: 400 });
-		}
+  async fetch(request: Request) {
+    const url = new URL(request.url);
+    if (request.headers.get('Upgrade') !== 'websocket') {
+      return new Response('Not a WebSocket request', { status: 400 });
+    }
 
-		const { 0: client, 1: server } = new WebSocketPair();
-		this.clients.add(server);
+    const { 0: client, 1: server } = new WebSocketPair();
+    this.clients.add(server);
 
-		server.accept();
-		server.addEventListener('message', (message) => this.broadcast(message, server));
-		server.addEventListener('close', () => this.clients.delete(server));
+    server.accept();
+    server.addEventListener('message', (message) =>
+      this.broadcast(message, server),
+    );
+    server.addEventListener('close', () => this.clients.delete(server));
 
-		return new Response(null, { status: 101, webSocket: client });
-	}
+    return new Response(null, { status: 101, webSocket: client });
+  }
 
-	broadcast(message: MessageEvent, server: WebSocket) {
-		console.log({ data: message.data });
-		const { data } = message;
-		const clients = [...this.clients].filter((client) => client !== server);
-		clients.forEach((client) => {
-			if (client.readyState === WebSocket.OPEN) {
-				client.send(data);
-			}
-		});
-	}
+  /**
+   * Here we do:
+   * 1. send the updating to the REST API then broadcasting the updated task
+   * 	if got any error, abandon the broadcast
+   * 2. broadcasting deleting task
+   * 3. broadcasting adding task
+   *
+   * @param message
+   * @param server
+   */
+  broadcast(message: MessageEvent, server: WebSocket) {
+    try {
+      const { data } = message;
+      console.log('[Broadcast] receiving message: ', data);
+
+      // only proceed JSON string message
+      if (typeof data !== 'string') return;
+
+      const parsedData = JSON.parse(data);
+
+      const { cmd, payload } = parsedData;
+      const operation = operations[cmd as Command];
+      // invalid command
+      if (!operation) return;
+      const clients = [...this.clients].filter(
+        (client) => client !== server && client.readyState === WebSocket.OPEN,
+      );
+      operation(payload, clients);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- co-tasker-stream/src/index.ts
+++ co-tasker-stream/src/index.ts
@@ -1,3 +1,4 @@
+import { validate } from 'uuid';
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -12,25 +13,29 @@
 // our Durable Object namespace, we must export it from the root module.
 export { Streamer } from './Streamer';
 
-import template from './template';
-
 const modules = {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		try {
-			const url = new URL(request.url);
-			switch (url.pathname) {
-				case '/':
-					return template();
-				case '/ws':
-					const id = env.Streamer.idFromName('co-tasker-streamer');
-					const stub = env.Streamer.get(id);
-					return stub.fetch(request);
-				default:
-					return new Response('Not found', { status: 404 });
-			}
-		} catch (err) {
-			return new Response(JSON.stringify({ error: err }));
-		}
-	},
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    try {
+      const { pathname } = new URL(request.url);
+      const [, , todo_uuid] = pathname.split('/');
+      if (!todo_uuid)
+        return new Response(JSON.stringify({ error: 'Not found' }), {
+          status: 404,
+        });
+
+      const isValid = validate(todo_uuid);
+      if (!isValid)
+        return new Response(JSON.stringify({ error: 'Invalid `todo_uuid`' }), {
+          status: 404,
+        });
+
+      const id = env.Streamer.idFromName(`co-tasker-streamer-${todo_uuid}`);
+      const stub = env.Streamer.get(id);
+      return stub.fetch(request);
+    } catch (err) {
+      return new Response(JSON.stringify({ error: err }));
+    }
+  },
 };
+
 export default modules;
