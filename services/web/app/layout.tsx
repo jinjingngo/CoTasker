d3e816713c9d280d17f9e2ea4e7db2559728d3f5
Jinjing Wu <jinjing.wu@compass.com>
2024-08-24 22:21:38 +0200

    feat: material ui

--- services/web/app/layout.tsx
+++ services/web/app/layout.tsx
@@ -1,6 +1,11 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 import './globals.css';
+
+import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
